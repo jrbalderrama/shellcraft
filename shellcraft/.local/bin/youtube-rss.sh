83e6d70891a09cee25247e3d4d5aa3e517165f38
jrbalderrama <jrbalderrama@users.noreply.github.com>
2024-01-17 15:54:48 +0100

    rewrite by simplifying code

--- shellcraft/.local/bin/youtube-rss.sh
+++ shellcraft/.local/bin/youtube-rss.sh
@@ -1,91 +1,31 @@
 #!/usr/bin/env bash
-# Purpose: 	get the RSS feed for a YouTube page
-# Inspired By: 	https://eggfreckles.net/notes/youtube-rss/
-# Gist: 	https://gist.github.com/tjluoma/fdbc63ceb78a2aecd3d638fd18b6ec6e
-#
-# From:	Timothy J. Luoma
-# Mail:	luomat at gmail dot com
-# Date:	2020-01-17; updated 2021-01-10
 
-# 2021-01-10 YouTube currently has both 'rssUrl' and
-# 	link rel="alternate" type="application/rss+xml" title="RSS" href="…"
-# 	which I do not believe where there a year ago when I first wrote this
-# 	I am looking for the latter, and if I don't find it, I fallback to the
-# 	former. If neither are found, an error is reported to the user.
-#
-# 2021-01-10 (Update 2): The previous comment only applies if you are looking
-#	at a channel page, i.e. "https://www.youtube.com/512pixels" but NOT if you
-#	are looking an an individual video such as 'https://www.youtube.com/watch?v=gQgSdwkvaDg'
-#
-# Version 2021-01-10.2 -- This script _should_ now work on either of those kinds of pages
+# Inspired by:
+# https://gist.github.com/tjluoma/fdbc63ceb78a2aecd3d638fd18b6ec6e
 
-NAME="$0:t:r"
+#NAME=$(basename $BASH_SOURCE)
 
-if [[ -e "$HOME/.path" ]]
-then
-	source "$HOME/.path"
-else
-	PATH="/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin"
-fi
+yt-rss() {
+    local CURL_FLAGS="--fail --silent --location --show-error"
+    # htmlq is installed with 'cargo'
+    LINK_RSS=$(curl $CURL_FLAGS ${1} \
+                   | htmlq 'link[type="application/rss+xml"]' -a href)
 
-COUNT='0'
+    if [[ -z $LINK_RSS ]];	then
+        CHANNEL_ID=$(curl $CURL_FLAGS ${1} \
+                         | grep -Po '(?<=("channelId":")).*?(?=")' \
+                         | head -1)
 
-for URL in "$@"
-do
+        if [[ -n $CHANNEL_ID ]]; then
+            LINK_RSS="https://www.youtube.com/feeds/videos.xml?channel_id=$CHANNEL_ID"
+        else
+            echo "$NAME: 'CHANNEL_ID' and 'LINK_RSS' are both empty for '${1}'." >&2
+            exit 1
+        fi
+    fi
 
-	LINK_REL=$(curl -sfLS "${URL}" \
-				| tr '<|>|,|\r' '\n' \
-				| awk -F'"' '/application\/rss\+xml/{print $8}')
+    # copy URL to clipboard
+    echo ${LINK_RSS} | tee >(wl-copy)
+}
 
-	if [[ "$LINK_REL" == "" ]]
-	then
-
-## Do Not Indent
-CHANNEL_ID=$(curl -sfLS "$URL" \
-| sed -e 's#<#\
-<#g' \
--e 's#>#>\
-#g' \
-| awk -F'"' '/meta itemprop="channelId"/{print $4}')
-## END - Do Not Indent
-
-		if [[ "$CHANNEL_ID" == "" ]]
-		then
-
-			echo "$NAME: '\$CHANNEL_ID' and '\$LINK_REL' are both empty for '${URL}'." >>/dev/stderr
-
-			((COUNT++))
-
-			continue
-
-		else
-
-			FEED="https://www.youtube.com/feeds/videos.xml?channel_id=$CHANNEL_ID"
-
-		fi
-
-	else
-		FEED="$LINK_REL"
-	fi
-
-	echo "$FEED"
-
-			# copy URL to clipboard
-	echo -n "$FEED" | wl-copy
-
-done
-
-if [[ "$COUNT" == "0" ]]
-then
-	# echo "$NAME: No errors"
-	exit 0
-elif [[ "$COUNT" == "1" ]]
-then
-	echo "$NAME: One error"
-else
-	echo "$NAME: $COUNT errors"
-fi
-
-exit $COUNT
-
-#EOF
+export -f yt-rss
